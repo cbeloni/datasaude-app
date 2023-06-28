81ab933f7f8f174d5c0d1162fbb0d19c3b1e3d45
Cauê Beloni <cbeloni@gmail.com>
2023-06-28 20:11:08 -0300

    Add título tabela poluentes

--- src/views/TableList.js
+++ src/views/TableList.js
@@ -22,10 +22,7 @@ function TableList() {
           <Col md="12">
             <Card className="strpied-tabled-with-hover">
               <Card.Header>
-                <Card.Title as="h4">Striped Table with Hover</Card.Title>
-                <p className="card-category">
-                  Here is a subtitle for this table
-                </p>
+                <Card.Title as="h4">Lista de poluentes</Card.Title>                
               </Card.Header>
               <Card.Body className="table-full-width table-responsive px-4">
                 <DataTableComponent></DataTableComponent>
