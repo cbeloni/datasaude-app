7130bc3708dea422a132073ce91495b0c1ed711c
Cauê Beloni <cbeloni@gmail.com>
2023-07-24 22:46:30 -0300

    add dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ChartistGraph from "react-chartist";
 // react-bootstrap components
 import {
@@ -15,12 +15,68 @@ import {
   OverlayTrigger,
   Tooltip,
 } from "react-bootstrap";
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Dashboard() {
+
+  const [data, setData] = useState();
+
+  axios.put('http://datasaude-api.beloni.dev.br/api/v1/poluentes/cetesb?persist=false')
+    .then((response) => {
+      // Armazene a resposta da API no estado
+      setData(response.data);
+    })
+    .catch((error) => {
+      // Imprima o erro
+      console.log(error);
+    });
+
   return (
-    <>
-      <h4>construindo...</h4>
-    </>
+    <div>
+      {data && (
+        <Row>          
+          {data.Payload.map((card) => {
+            const variante = {
+              'N1 - BOA': 'success',
+              'N2 - MODERADA': 'warning',
+            }[card.qualidade];
+
+            
+            
+            return (    
+              <Col md="3" key={card.id}>
+                <Card
+                  bg={variante || 'danger'}
+                  key={card.id}
+                  text='white'
+                  style={{ width: '18rem' }}
+                  className="mb-2"
+                >
+                <Card.Body align="center">
+                    <Card.Text>
+                    <strong> {card.nome} </strong>
+                    <hr></hr>
+                    </Card.Text>
+                    <Card.Text>
+                      Qualidade: {card.qualidade} 
+                    </Card.Text>
+                    <Card.Text>
+                      Indice: {card.indice}
+                    </Card.Text>                             
+                  </Card.Body>
+                  <Card.Footer align="center">
+                  <div>
+                    <i className="fas fa-redo mr-1"></i>
+                    Atualizado: {card.data}
+                  </div>
+                </Card.Footer>
+                </Card>
+              </Col>
+          )})}
+        </Row>
+      )}
+    </div>
   );
 }
 
