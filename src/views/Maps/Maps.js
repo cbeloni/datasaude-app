326cd4d846a68e43f043c737416dbab2c2b52b13
Cauê Beloni <cbeloni@gmail.com>
2023-07-30 17:54:03 -0300

    add map

--- src/views/Maps/Maps.js
+++ src/views/Maps/Maps.js
@@ -1,100 +1,95 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "leaflet/dist/leaflet.css";
+import "leaflet/dist/images/marker-icon.png";
+import "leaflet/dist/images/marker-shadow.png";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+import "leaflet-defaulticon-compatibility";
+import L from "leaflet";
+import { Box, Slider } from "@material-ui/core";
 
 const Maps = () => {
-  const mapRef = React.useRef(null);
-  React.useEffect(() => {
-    let google = window.google;
-    let map = mapRef.current;
-    let lat = "40.748817";
-    let lng = "-73.985428";
-    const myLatlng = new google.maps.LatLng(lat, lng);
-    const mapOptions = {
-      zoom: 12,
-      center: myLatlng,
-      scrollwheel: false,
-      zoomControl: true,
-      styles: [
-        {
-          featureType: "water",
-          stylers: [{ saturation: 43 }, { lightness: -11 }, { hue: "#0088ff" }],
-        },
-        {
-          featureType: "road",
-          elementType: "geometry.fill",
-          stylers: [
-            { hue: "#ff0000" },
-            { saturation: -100 },
-            { lightness: 99 },
-          ],
-        },
-        {
-          featureType: "road",
-          elementType: "geometry.stroke",
-          stylers: [{ color: "#808080" }, { lightness: 54 }],
-        },
-        {
-          featureType: "landscape.man_made",
-          elementType: "geometry.fill",
-          stylers: [{ color: "#ece2d9" }],
-        },
-        {
-          featureType: "poi.park",
-          elementType: "geometry.fill",
-          stylers: [{ color: "#ccdca1" }],
-        },
-        {
-          featureType: "road",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#767676" }],
-        },
-        {
-          featureType: "road",
-          elementType: "labels.text.stroke",
-          stylers: [{ color: "#ffffff" }],
-        },
-        { featureType: "poi", stylers: [{ visibility: "off" }] },
-        {
-          featureType: "landscape.natural",
-          elementType: "geometry.fill",
-          stylers: [{ visibility: "on" }, { color: "#b8cb93" }],
-        },
-        { featureType: "poi.park", stylers: [{ visibility: "on" }] },
-        {
-          featureType: "poi.sports_complex",
-          stylers: [{ visibility: "on" }],
-        },
-        { featureType: "poi.medical", stylers: [{ visibility: "on" }] },
-        {
-          featureType: "poi.business",
-          stylers: [{ visibility: "simplified" }],
-        },
-      ],
-    };
+  useEffect(() => {
+    const map = L.map("map").setView([-23.6226, -46.5489], 9);
 
-    map = new google.maps.Map(map, mapOptions);
+    // const iconMarker = L.icon({
+    //   iconUrl: '/images/marker-icon.png',
+    //   iconSize: [25, 41],
+    //   iconAnchor: [12, 41],
+    //   popupAnchor: [0, -41],
+    // });
 
-    const marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      animation: google.maps.Animation.DROP,
-      title: "Material Dashboard React!",
-    });
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-    const contentString =
-      '<div class="info-window-content"><h2>Material Dashboard React</h2>' +
-      "<p>A premium Admin for React, Material-UI, and React Hooks.</p></div>";
+    L.marker([-23.40563825, -46.30159285])
+      .addTo(map)
+      .bindPopup(
+        "ESTRADA DOS INDIOS DE 1 A 99999 2645 CHACARAS COPACO 7439060"
+      );
+    L.marker([-23.5225, -46.63918998])
+      .addTo(map)
+      .bindPopup("RUA ADORACAO 67 BOM RETIRO 1128080");
+    L.marker([-23.53437107, -46.72766621])
+      .addTo(map)
+      .bindPopup(
+        "AVENIDA IMPERATRIZ LEOPOLDINA DE 1585 A 99999 1845 VILA LEOPOLDINA 5305007"
+      );
+  }, []);
 
-    const infowindow = new google.maps.InfoWindow({
-      content: contentString,
-    });
+  const marks = [
+    {
+      value: 0,
+      label: "Jan",
+    },
+    {
+      value: 10,
+      label: "Fev",
+    },
+    {
+      value: 20,
+      label: "Mar",
+    },
+    {
+      value: 30,
+      label: "Abr",
+    },
+    {
+      value: 40,
+      label: "Mai",
+    },
+    {
+      value: 50,
+      label: "Jun",
+    },
+    {
+      value: 60,
+      label: "Jul",
+    },
+    {
+      value: 100,
+      label: "Dez",
+    },
+  ];
 
-    google.maps.event.addListener(marker, "click", function () {
-      infowindow.open(map, marker);
-    });
-  });
+  function valuetext(value) {
+    return `${value}`;
+  }
+
   return (
     <>
-      <div style={{ height: `100vh` }} ref={mapRef}></div>
+      <Box sx={{ width: 300, paddingLeft: "30px" }}>
+        <Slider
+          aria-label="Custom marks"
+          defaultValue={20}
+          getAriaValueText={valuetext}
+          step={10}
+          valueLabelDisplay="auto"
+          marks={marks}
+        />
+      </Box>
+      <div id="map" style={{ height: "80vh" }}></div>
     </>
   );
 };
