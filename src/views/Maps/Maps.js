378475c7c66b75099c95dcd77907d205061df2de
Cauê Beloni <cbeloni@gmail.com>
2023-08-16 22:01:57 -0300

    add card to maps

--- src/views/Maps/Maps.js
+++ src/views/Maps/Maps.js
@@ -1,14 +1,29 @@
 import React from "react";
-import GridItem from "components/Grid/GridItem";
-import GridContainer from "components/Grid/GridContainer";
 import IframeTable from "components/Table/IframeTable";
+import GridContainer from "components/Grid/GridContainer";
+import GridItem from "components/Grid/GridItem";
+import Card from "components/Card/Card";
+import CardHeader from "components/Card/CardHeader";
+import CardBody from "components/Card/CardBody";
+import { makeStyles } from "@material-ui/core/styles";
+import styles from "layouts/Styles/commom.js";
 
+const useStyles = makeStyles(styles);
 const Maps = () => {
+  const classes = useStyles();
   return (
     <>
       <GridContainer>
-        <GridItem xs={12}>
-          <IframeTable></IframeTable>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card>
+            <CardHeader color="primary">
+              <h4 className={classes.cardTitleWhite}>Mapa poluente MP10</h4>
+              <p className={classes.cardCategoryWhite}>Média poluentes 2023</p>
+            </CardHeader>
+            <CardBody>
+              <IframeTable></IframeTable>
+            </CardBody>
+          </Card>
         </GridItem>
       </GridContainer>
     </>
--- src/views/TableList/TableList.js
+++ src/views/TableList/TableList.js
@@ -13,36 +13,7 @@ import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
-
-const styles = {
-  cardCategoryWhite: {
-    "&,& a,& a:hover,& a:focus": {
-      color: "rgba(255,255,255,.62)",
-      margin: "0",
-      fontSize: "14px",
-      marginTop: "0",
-      marginBottom: "0",
-    },
-    "& a,& a:hover,& a:focus": {
-      color: "#FFFFFF",
-    },
-  },
-  cardTitleWhite: {
-    color: "#FFFFFF",
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none",
-    "& small": {
-      color: "#777",
-      fontSize: "65%",
-      fontWeight: "400",
-      lineHeight: "1",
-    },
-  },
-};
+import styles from "layouts/Styles/commom.js";
 
 const useStyles = makeStyles(styles);
 
