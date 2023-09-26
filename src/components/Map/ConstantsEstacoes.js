e8a00826d61d85860ca3a630c147a52be778015c
Cauê Beloni <cbeloni@gmail.com>
2023-09-25 22:43:01 -0300

    correção latitud

--- src/components/Map/ConstantsEstacoes.js
+++ src/components/Map/ConstantsEstacoes.js
@@ -1,139 +1,139 @@
 const estacoes = [
   {
     endereco: "Rua Benjamin Constant, 3, Pref. Mun. de Diadema, Vila Diadema",
-    localidade: [-236889637, -46.6041867],
+    localidade: [-23.6889637, -46.6041867],
   },
   {
     endereco:
       "Alameda dos Tupiniquins, 1571, E.M.Prof.J.C. da Silva Borges, Planalto Paulista",
-    localidade: [-236160321, -46.6633094],
+    localidade: [-23.6160321, -46.6633094],
   },
   {
     endereco:
       "Rua Padre José Maria, 555, Centro Ed. e Esp. Mun. Joerg Brüder, Santo Amaro",
-    localidade: [-236548192, -46.7096562],
+    localidade: [-23.6548192, -46.7096562],
   },
   {
     endereco:
       "Av. Santos Dumont, 1019, Parque de Material Aeronáutico, Santana",
-    localidade: [-235154239, -46.62933],
+    localidade: [-23.5154239, -46.62933],
   },
   {
     endereco: "Rua dos Vianas, 625, Vila Baeta Neves",
-    localidade: [-236993401, -46.547356],
+    localidade: [-23.6993401, -46.547356],
   },
   {
     endereco:
       "Av. Prof. Frederico Herman Júnior, 345, CETESB, Alto de Pinheiros",
-    localidade: [-2356119, -46.7015441],
+    localidade: [-23.56119, -46.7015441],
   },
   {
     endereco: "Estrada São Paulo-Jundiaí, 1666, Perus",
-    localidade: [-234150815, -46.75642029999999],
+    localidade: [-23.4150815, -46.75642029999999],
   },
   {
     endereco:
       "Rua Bresser, 2341, Centro Esportivo Municipal – Subprefeitura da Moóca, Moóca",
-    localidade: [-235493885, -46.6014959],
+    localidade: [-23.5493885, -46.6014959],
   },
   {
     endereco:
       "Av. Fernando do Espírito Santo Alves de Mattos, 1000, SESC Itaquera, Prq do Carmo",
-    localidade: [-235825681, -46.470467],
+    localidade: [-23.5825681, -46.470467],
   },
   {
     endereco:
       "Rua Jaguar, 225, Bibl. Mun. Vicente Paulo Guimarães, Vila Curuçá",
-    localidade: [-235017463, -46.4206558],
+    localidade: [-23.5017463, -46.4206558],
   },
   {
     endereco: "Rua Domingas Galleteri Blotta, 171, Jd Santa Cruz",
-    localidade: [-236808488, -46.6757783],
+    localidade: [-23.6808488, -46.6757783],
   },
   {
-    endereco: "Parque do Ibirapuera, s\\/nº, Setor 25, Prq Ibirapuera",
-    localidade: [-235874162, -46.6576336],
+    endereco: "Parque do Ibirapuera, s/nº, Setor 25, Prq Ibirapuera",
+    localidade: [-23.5874162, -46.6576336],
   },
   {
     endereco:
       "Rua Imperial, 230, Hospital Municipal Pimental-Bonsucesso, Prq das Nações",
-    localidade: [-234400857, -46.41011959999999],
+    localidade: [-23.4400857, -46.41011959999999],
   },
   {
     endereco: "strada de Itapecerica, 5859, Campus UNASP, Capão Redondo",
-    localidade: [-236662666, -46.7831266],
+    localidade: [-23.6662666, -46.7831266],
   },
   {
     endereco: "v. Inocêncio Seráfico, 1051, Reservatório da SABESP, Centro",
-    localidade: [-235301409, -46.8354937],
+    localidade: [-23.5301409, -46.8354937],
   },
   {
     endereco: "Av. Doutor Arnaldo, 725, Faculdade de Saúde Pública, Sumaré",
-    localidade: [-235535158, -46.673472],
+    localidade: [-23.5535158, -46.673472],
   },
   {
     endereco:
       "Av. Professor Lineu Prestes, 2242, Cidade Universitária, Butantã",
-    localidade: [-235666823, -46.7277265],
+    localidade: [-23.5666823, -46.7277265],
   },
   {
     endereco: "Rua Salgado Filho, 121, Prq. Fernando Jorge",
-    localidade: [-238795648, -46.4182329],
+    localidade: [-23.8795648, -46.4182329],
   },
   {
     endereco:
       "v. Paulo Guilguer Reimberg, 2448, E.E. Pres. Tancredo A. Neves, Jd Novo Horizonte",
-    localidade: [-219727932, -46.81680859999999],
+    localidade: [-21.9727932, -46.81680859999999],
   },
   {
     endereco: "Av. João Bernardo Medeiros, 173, Paço Municipal, Jd Bom Clima",
-    localidade: [-234558697, -46.5183867],
+    localidade: [-23.4558697, -46.5183867],
   },
   {
     endereco:
       "Av. Embaixador Macedo Soares, 12889, Centro de Trein. da Polícia Militar, Vl Ribeiro de Barros",
-    localidade: [-235186189, -46.7430349],
+    localidade: [-23.5186189, -46.7430349],
   },
   {
-    endereco: "Rua Vitorino Dell Antônia, s\\/nº, Paço Municipal, Vila Noêmia",
-    localidade: [-236692225, -46.4650134],
+    endereco: "Rua Vitorino Dell Antônia, s/nº, Paço Municipal, Vila Noêmia",
+    localidade: [-23.6692225, -46.4650134],
   },
   {
     endereco:
       "Rua Capitão José Amaral, 80, E.E. Cacilda Becker, Vila Portuguesa",
-    localidade: [-234799634, -46.69236979999999],
+    localidade: [-23.4799634, -46.69236979999999],
   },
   {
     endereco:
-      "Av. dos Autonomistas, s\\/nº, Esquina com Rua São Mauricio, Vila Quitaúna",
-    localidade: [-235264022, -46.7917002],
+      "Av. dos Autonomistas, s/nº, Esquina com Rua São Mauricio, Vila Quitaúna",
+    localidade: [-23.5264022, -46.7917002],
   },
   {
-    endereco: "Parque Dom Pedro II, s\\/nº, Palácio das Indústrias, Centro",
-    localidade: [-235476409, -46.6303335],
+    endereco: "Parque Dom Pedro II, s/nº, Palácio das Indústrias, Centro",
+    localidade: [-23.5476409, -46.6303335],
   },
   {
     endereco:
-      "Estrada Turística do Jaraguá, s\\/nº, Altura do n. 9000, Parque Est. do Jaraguá, Vila Jaraguá",
-    localidade: [-23469316, -46.7542667],
+      "Estrada Turística do Jaraguá, s/nº, Altura do n. 9000, Parque Est. do Jaraguá, Vila Jaraguá",
+    localidade: [-23.469316, -46.7542667],
   },
   {
     endereco: "Rua Manágua, 2, Posto Pueric. Alto Capuava, Prq Capuava",
-    localidade: [-236415859, -46.4919545],
+    localidade: [-23.6415859, -46.4919545],
   },
   {
     endereco:
       "Rua Xavier de Toledo, 521, EMEB Arlindo Ferreira, Vila Paulicéia",
-    localidade: [-23670996, -46.5850029],
+    localidade: [-23.670996, -46.5850029],
   },
   {
     endereco:
       "Presidente Kennedy, 700, Hosp. Mun. de Emerg. Albert Sabin, Santa Paula",
-    localidade: [-236176297, -46.5668298],
+    localidade: [-23.6176297, -46.5668298],
   },
   {
     endereco: "Praça Nicola Vivilechio, 99, Jd Bom Tempo",
-    localidade: [-236092398, -46.7559152],
+    localidade: [-23.6092398, -46.7559152],
   },
 ];
 
