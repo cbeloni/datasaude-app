4211009e08a78f69a00ba30f4bbdb024370fcec7
caue beloni <cbeloni@gmail.com>
2024-07-06 18:25:23 -0300

    fix eslint

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
 
 const apiURL = "https://datasaude-api.beloni.dev.br/api/v1/users/login";
@@ -24,26 +23,3 @@ export const login = async (email, password) => {
 export const isAuthenticated = () => {
   return !!localStorage.getItem("authToken");
 };
-
-export const useAuth = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const checkAuth = () => {
-      const authToken = localStorage.getItem("authToken");
-      setIsAuthenticated(!!authToken);
-    };
-
-    checkAuth();
-
-    window.addEventListener("storage", checkAuth);
-
-    return () => {
-      window.removeEventListener("storage", checkAuth);
-    };
-  }, []);
-
-  return isAuthenticated;
-};
-
-export default useAuth;
